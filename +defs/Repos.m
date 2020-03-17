5d1a74318674ee337ce87da31e8b5b0ae6635b65
Max_KUMC_Desktop <max.murphy11@gmail.com>
2020-03-17 18:06:12 -0500

    Update "helper" repos paths

--- +defs/Repos.m
+++ +defs/Repos.m
@@ -1,8 +1,9 @@
 function repos = Repos()
+%REPOS  List of repositories to add to path alongside analysis code
 
 repos = struct;
-repos.CBREWER_PATH = 'C:\MyRepos\_import\cbrewer';
-repos.ROBUST_STATISTICS_PATH = 'C:\MyRepos\_import\Robust_Statistical_Toolbox';
-repos.RAINCLOUDPLOTS_PATH = 'C:\MyRepos\_import\RainCloudPlots';
+repos.CBREWER_PATH = 'C:\MyRepos\_import\cbrewer_tDCS';
+repos.ROBUST_STATISTICS_PATH = 'C:\MyRepos\_import\Robust_Statistical_Toolbox_tDCS';
+repos.RAINCLOUDPLOTS_PATH = 'C:\MyRepos\_import\RainCloudPlots_tDCS';
 
 end
